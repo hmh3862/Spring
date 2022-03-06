b4400febb2c99f7278088af3d82f1efb1b7999f1
Moonhee Hong <hmh94253862@gmail.com>
2022-03-06 11:09:14 +0900

    BootEx02_HelloWorld_Thymeleaf

---
+++ BootEx02_HelloWorld_Thymeleaf/src/main/java/kr/green/boot/BootEx02HelloWorldThymeleafApplication.java
@@ -0,0 +1,13 @@
+package kr.green.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootEx02HelloWorldThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootEx02HelloWorldThymeleafApplication.class, args);
+	}
+
+}
