fc76e67b8059a6f823fea5b060ac27c9b1a38462
Moonhee Hong <hmh94253862@gmail.com>
2022-03-08 02:39:31 +0900

    BootEx06_MybatisCRUD

---
+++ BootEx06_MybatisCRUD/src/main/java/kr/green/boot/dao/TestDAO.java
@@ -0,0 +1,17 @@
+package kr.green.boot.dao;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.green.boot.vo.TestVO;
+
+@Mapper
+public interface TestDAO {
+	String today() throws SQLException;
+	int    sum(HashMap<String, Integer> map) throws SQLException;
+	int    mul(HashMap<String, Integer> map) throws SQLException;
+	TestVO vo(TestVO testVO) throws SQLException;
+}
+
